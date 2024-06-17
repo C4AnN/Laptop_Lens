5e90edb41bbf259f96be5ed1235bad4eb9877034
Cannn <aw45286@gmail.com>
2024-06-17 22:41:14 +0700

    Rev

--- MD/LaptopLens/app/src/main/java/com/example/laptoplens/ApiResponse.kt
+++ MD/LaptopLens/app/src/main/java/com/example/laptoplens/ApiResponse.kt
@@ -34,6 +34,11 @@ data class User(
     val confirmPassword: String
 )
 
+data class LogoutRequest(
+    val email: String,
+    val password: String
+)
+
 data class LoginRequest(
     val email: String,
     val password: String
